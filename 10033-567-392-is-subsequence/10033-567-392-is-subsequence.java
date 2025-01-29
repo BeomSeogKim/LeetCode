6873e03180b586bfeeb6a17517a8bd2e46c2820c
Tommy-Kim <110332047+BeomSeogKim@users.noreply.github.com>
2025-01-29 17:14:57 +0900

    Time: 1 ms, Memory: 41.9 MB - LeetHub

---
+++ 10033-567-392-is-subsequence/10033-567-392-is-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int leftBound = s.length();
+        int rightBound = t.length();
+        int pLeft = 0, pRight = 0;
+
+        while(pLeft < leftBound && pRight < rightBound) {
+            if (s.charAt(pLeft) == t.charAt(pRight)) {
+                pLeft++;
+            }
+            pRight++;
+        }
+
+        return pLeft == leftBound;
+    }
+}
